6095766c3d3bd984b9bb4b4c61eb6c866c6050d4
gaubert <guillaume@aubert.ws>
2022-03-01 12:20:38 +0100

    Handling $ for tests and handling ^\ ^D ^C

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gaubert <gaubert@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 13:00:58 by gaubert           #+#    #+#             */
-/*   Updated: 2022/02/24 18:05:48 by gaubert          ###   ########.fr       */
+/*   Updated: 2022/03/01 12:18:13 by gaubert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@ void	handle_signals(int signo)
 {
 	if (signo == SIGINT)
 	{
+		write(1, "\b\b  ", 1);
 		write(1, "\n", 1);
 		rl_replace_line("", 0);
+		rl_on_new_line();
+		rl_redisplay();
+	}
+	if (signo == SIGQUIT)
+	{
 		rl_on_new_line();
 		rl_redisplay();
 	}
@@ -34,9 +40,9 @@ int	main(void)
 	char	*line;
 
 	if (signal(SIGINT, handle_signals) == SIG_ERR)
-	{
 		printf("failed to register interrupts with kernel\n");
-	}
+	if (signal(SIGQUIT, handle_signals) == SIG_ERR)
+		printf("failed to register interrupts with kernel\n");
 	line = readline ("$>");
 	while (line != NULL)
 	{
@@ -44,6 +50,10 @@ int	main(void)
 		printf("%s\n", line);
 		if (ft_strncmp("exit", line, 5) == 0)
 			exit(0);
+		if (ft_strncmp("$", line, 1) == 0)
+		{
+			env(line + 1);
+		}
 		disp_next_prompt(&line);
 	}
 }
